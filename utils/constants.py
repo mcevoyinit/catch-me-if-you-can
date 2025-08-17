73029d2a707719d4f6eae7a56addbbc8c9cb85d1
mcevoyinit <ericmcevoy@protonmail.com>
2025-08-17 14:01:42 +0100

    feat: Add utils module with constants configuration

---
+++ utils/constants.py
@@ -0,0 +1,22 @@
+"""
+Constants and configuration values
+"""
+
+# Decision thresholds
+EXECUTE_THRESHOLD = 0.3
+CAUTION_THRESHOLD = 0.1
+
+# Risk zones
+RISK_GREEN = 0.3
+RISK_YELLOW = 0.6
+
+# Jurisdictions
+SUPPORTED_JURISDICTIONS = ["US", "EU", "UK", "SG", "AE", "PT", "JP", "CN", "IN", "BR"]
+
+# Time constants
+DEFAULT_MONTHS = 12
+MAX_MONTHS = 60
+
+# Financial constants
+MIN_INVESTMENT = 1000
+MAX_PENALTY_RATIO = 2.0
